ec0f1aacd5f5e8d29f1ded11bf5a746e9f53b77e
tolgayilmazcomtr <65124854+tolgayilmazcomtr@users.noreply.github.com>
2026-02-17 02:00:02 +0300

    Use allowed_origins_patterns=* to fix CORS with credentials

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
